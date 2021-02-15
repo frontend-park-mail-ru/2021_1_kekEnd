0f473b0e3866daa1968dd7247c93c5042eb90b23
Алексей <zotov.alexei.1@yandex.ru>
2021-02-15 14:44:26 +0300

    fixes

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,15 +1,24 @@
-const loginSubmit = document.getElementById("login");
-const signupSubmit = document.getElementById("signup");
-const switcher = document.getElementById("btn");
+const loginSubmit = document.getElementById('login');
+const signupSubmit = document.getElementById('signup');
+const switcher = document.getElementById('btn');
 
-document.getElementById("signup-btn").addEventListener("click", function signup() {
-    loginSubmit.style.left = "-400px";
-    signupSubmit.style.left = "50px";
-    switcher.style.left = "120px";
+const signupButton = document.getElementById('signup-btn');
+const loginButton = document.getElementById('login-btn');
+
+signupButton.addEventListener('click', function signup() {
+    loginSubmit.style.left = '-400px';
+    signupSubmit.style.left = '50px';
+    switcher.style.left = '120px';
+
+    signupButton.style.color = 'white';
+    loginButton.style.color = 'black';
 });
 
-document.getElementById("login-btn").addEventListener("click", function login() {
-    loginSubmit.style.left = "50px";
-    signupSubmit.style.left = "450px";
-    switcher.style.left = "0px";
+loginButton.addEventListener('click', function login() {
+    loginSubmit.style.left = '50px';
+    signupSubmit.style.left = '450px';
+    switcher.style.left = '0px';
+
+    signupButton.style.color = 'black';
+    loginButton.style.color = 'white';
 });
