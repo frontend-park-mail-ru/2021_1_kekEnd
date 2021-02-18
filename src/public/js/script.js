7bfee7cbb7069cae138e7268e2d0bd9333e8ace3
Алексей <zotov.alexei.1@yandex.ru>
2021-02-19 01:41:32 +0300

    using classList in script.js

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -6,22 +6,15 @@ const signupButton = document.getElementById('signup-button');
 const loginButton = document.getElementById('login-button');
 
 // TODO: add additional logic: switch either to 'Login' or to 'Sign Up'
-loginButton.style.color = 'white';
+loginButton.classList.add('button-white-text');
 
-signupButton.addEventListener('click', () => {
-    loginSubmit.style.left = '-400px';
-    signupSubmit.style.left = '50px';
-    switcher.style.left = '120px';
+const moveForms = () => {
+    loginSubmit.classList.toggle('login-form-inactive');
+    signupSubmit.classList.toggle('signup-form-active');
+    switcher.classList.toggle('switcher-on-signup');
+    signupButton.classList.toggle('button-white-text');
+    loginButton.classList.toggle('button-white-text');
+}
 
-    signupButton.style.color = 'white';
-    loginButton.style.color = 'black';
-});
-
-loginButton.addEventListener('click', () => {
-    loginSubmit.style.left = '50px';
-    signupSubmit.style.left = '450px';
-    switcher.style.left = '0px';
-
-    signupButton.style.color = 'black';
-    loginButton.style.color = 'white';
-});
+signupButton.addEventListener('click', moveForms);
+loginButton.addEventListener('click', moveForms);
