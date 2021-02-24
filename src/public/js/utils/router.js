a049086417a19f96b8c650959b7bd9d04122d7d8
Алексей <zotov.alexei.1@yandex.ru>
2021-02-24 13:11:24 +0300

    router carcass

---
+++ src/public/js/utils/router.js
@@ -0,0 +1,27 @@
+'use strict';
+
+class Router {
+    constructor() {
+
+    }
+
+    register(path, view) {
+
+    }
+
+    start() {
+        // запустить роутер
+    }
+
+    go(path) {
+
+    }
+
+    back() {
+
+    }
+
+    forward() {
+
+    }
+}
