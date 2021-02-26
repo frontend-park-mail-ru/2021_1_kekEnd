3305581ee661d93d6eef35a8ee75fd38f462871e
Алексей <zotov.alexei.1@yandex.ru>
2021-02-26 16:55:40 +0300

    delete useless BaseController

--- src/public/js/controllers/ProfileController.js
+++ src/public/js/controllers/ProfileController.js
@@ -1,17 +1,9 @@
-import BaseController from './BaseController.js';
 import ProfileModel from '../models/ProfileModel.js';
 import ProfileView from '../views/ProfileView.js';
 
-
-export default class ProfileController extends BaseController {
+export default class ProfileController {
     constructor(parent) {
-        super();
         this.view = new ProfileView(parent);
         this.model = new ProfileModel();
-    }
-
-    activate() {
-        // start business logic here
-        this.view.render();
     }
 }
