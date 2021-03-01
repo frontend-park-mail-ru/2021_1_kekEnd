c4ce966d51d85ce7f118c31b4ee9d447a08b0cfe
Ilya Afimin <fur1ed@icloud.com>
2021-03-02 01:05:01 +0300

    Добавлено: шаблонизотор

--- src/public/js/components/baseView.js
+++ src/public/js/components/baseView.js
@@ -3,13 +3,13 @@
 
 export default class BaseView {
     constructor(parent, template) {
-    	this.parent = parent;
+        this.parent = parent;
         this.template = template;
         this.root = '';
     }
 
     render(root, data) {
-    	this.root = root;
-    	this.root.innerHTML = this.template(data);
+        this.root = root;
+        this.root.innerHTML = this.template(data);
     }
 }
--- src/public/js/utils/validation.js
+++ src/public/js/utils/validation.js
@@ -1,7 +1,8 @@
 'use strict';
 
-class Validator {
 
+// eslint-disable-next-line no-unused-vars
+class Validator {
     validateEmail(email) {
         const error = [];
 
@@ -16,7 +17,7 @@ class Validator {
         const error = [];
 
         if (!(login.length >= 6 && login.length <= 20)) {
-            error.push('should be from 6 to 20 characters long')
+            error.push('should be from 6 to 20 characters long');
         }
         if (!(/^[a-zA-Z0-9_]*$/.test(login))) {
             error.push('should contain only latin letters, numbers and underscores');
@@ -27,9 +28,9 @@ class Validator {
 
     validatePassword(password) {
         const error = [];
-        
+
         if (!(password.length >= 8 && password.length <= 32)) {
-            error.push('should be from 8 to 32 characters long')
+            error.push('should be from 8 to 32 characters long');
         }
         if (!(/\d/.test(password))) {
             error.push('should contain at least one digit');
@@ -37,9 +38,8 @@ class Validator {
         if (!(/[a-zA-Z]/.test(password))) {
             error.push('should contain at least one letter');
         }
-        
+
         return error;
     }
-
 }
 
