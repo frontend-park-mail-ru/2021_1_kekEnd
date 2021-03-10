eee87e401350f618ba975a7ba8ae2262d36e06cc
Alexey Zotov <zotov.alexei.1@yandex.ru>
2021-03-10 15:53:30 +0400

    fix router

--- src/public/js/utils/router.js
+++ src/public/js/utils/router.js
@@ -15,37 +15,48 @@ export class Router {
     start() {
         window.addEventListener('load', () => {
             const path = location.pathname;
-            this.handlePath(path);
+            this.searchForParams(path);
         });
 
         window.addEventListener('popstate', (event) => {
             const path = event.target.location.pathname;
             event.preventDefault();
-
-            this.pushState(path);
+            this.searchForParams(path);
         });
 
         window.addEventListener('click', (event) => {
             const {target} = event;
             const link = findAscendingTag(target, 'A');
             const path = (link !== null) ? link.href : null;
-            
-            for (const i in PATHS) {
-                if (path !== null && path.includes(PATHS[i])) {
-                    event.preventDefault();
-                    const parameters = path.substring(path.indexOf(PATHS[i]) + 1).substring(PATHS[i].length);
-                    this.pushState(PATHS[i], {}, parameters);
-                    break;
-                }
-            }
+
+            event.preventDefault();
+            this.searchForParams(path);
         });
     }
 
+    searchForParams(path) {
+        for (const i in PATHS) {
+            if (path !== null && path.includes(PATHS[i])) {
+                const parameters = path.substring(path.indexOf(PATHS[i]) + 1).substring(PATHS[i].length);
+                this.pushState(PATHS[i], {}, parameters);
+                break;
+            }
+        }
+    }
+
     pushState(path = '/', state = {}, parameters = '') {
-        if (path !== location.pathname) {
-            history.pushState(state, document.title, path);
+        let newState, newPath;
+        if (parameters) {
+            newState = `${state}/${parameters}`;
+            newPath = `${path}/${parameters}`;
         } else {
-            history.replaceState(state, document.title, path);
+            newState = state;
+            newPath = path;
+        }
+        if (path !== location.pathname) {
+            history.pushState(newState, document.title, newPath);
+        } else {
+            history.replaceState(newState, document.title, newPath);
         }
 
         this.handlePath(path, parameters);
