50053c6301b87a43583c589d3e385e0f64ac7936
grillow <spmr757@gmail.com>
2021-03-20 18:14:10 +0300

    model, controller

--- src/public/js/components/actor/actorModel.js
+++ src/public/js/components/actor/actorModel.js
@@ -24,7 +24,6 @@ export default class ActorModel {
      * @param {int} id - id актера
      */
     getActorData(id) {
-        /*
         API.getActorData(id)
             .then((res) => {
                 API.getUser()
@@ -33,8 +32,6 @@ export default class ActorModel {
                             {...res.data, 'isAuthorized': authRes.status === OK});
                     });
             });
-        */
-        console.log('actor data requested');
     }
 
     /**
