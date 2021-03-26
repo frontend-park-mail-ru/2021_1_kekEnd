441bfa94a9d3994e1e8a77f126c62c2c043faa61
grillow <spmr757@gmail.com>
2021-03-27 00:24:18 +0300

    path

--- src/public/js/utils/paths.js
+++ src/public/js/utils/paths.js
@@ -2,6 +2,7 @@ export const PATHS = {
     login: '/login',
     signup: '/signup',
     movie: '/movie',
+    actor: '/actor',
     settings: '/settings',
     profile: '/', // временно, пока не готовы главная страница и остальные контроллеры
 };
