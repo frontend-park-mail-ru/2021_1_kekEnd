0d7cfadd904392d93f0a9d7178786c6ff353827a
Alexey Zotov <zotov.alexei.1@yandex.ru>
2021-04-03 11:20:49 +0300

    fix paths order

--- src/public/js/utils/paths.js
+++ src/public/js/utils/paths.js
@@ -1,8 +1,8 @@
 export const PATHS = {
-    main: '/main',  // TODO: make it '/' after router is fixed
     login: '/login',
     signup: '/signup',
     movie: '/movie',
     settings: '/settings',
-    profile: '/me'
+    profile: '/me',
+    main: '/'
 };
