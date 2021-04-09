20c7a5ae10bfbbaf81f2d305b32409a37751266c
grillow <spmr757@gmail.com>
2021-04-09 23:35:18 +0300

    a click interseption

--- src/public/js/components/main/mainView.js
+++ src/public/js/components/main/mainView.js
@@ -95,7 +95,7 @@ export default class MainView extends BaseView {
         if (genres.length) {
             console.log('search by genres clicked: ' + genres);
             // /TODO: search by genres
-            document.getElementById('main-genre-search-button').href=`movies/genre/1/?filter=${genres.join('+')}`;
+            document.getElementById('main-genre-search-button').href=`/movies/genre/1/?filter=${genres.join('+')}`;
         } else {
             // return false;
         }
