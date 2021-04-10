99a2f7b20c3cec1ab248c6afb62a482c6f948c48
Ilya Afimin <fur1ed@icloud.com>
2021-04-10 22:16:03 +0300

    Выделены повторяющиеся части шаблонов

--- gentmpl.sh
+++ gentmpl.sh
@@ -5,3 +5,6 @@ handlebars src/public/js/components/movie/movie.hbs -f src/public/js/components/
 handlebars src/public/js/components/settings/settings.hbs -f src/public/js/components/settings/settings.tmpl.js
 handlebars src/public/js/components/login/login.hbs -f src/public/js/components/login/login.tmpl.js
 handlebars src/public/js/components/signup/signup.hbs -f src/public/js/components/signup/signup.tmpl.js
+
+handlebars src/public/js/components/partials/navbar.hbs -f src/public/js/components/partials/navbar.tmpl.js
+handlebars src/public/js/components/partials/top_login_signup.hbs -f src/public/js/components/partials/top_login_signup.tmpl.js
