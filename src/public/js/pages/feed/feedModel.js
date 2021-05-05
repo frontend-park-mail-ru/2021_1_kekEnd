59766aaf8989a12624c6b761216a388a5a22cfa6
Alexey Zotov <zotov.alexei.1@yandex.ru>
2021-05-05 21:43:44 +0400

    feed integration

--- src/public/js/pages/feed/feedModel.js
+++ src/public/js/pages/feed/feedModel.js
@@ -17,35 +17,11 @@ export default class FeedModel {
      * Получение ленты пользователя
      */
     getFeed() {
-        /*
-        feed: [
-                {
-                    id: 1,
-                    itemType: 'review',
-                    username: 'sampleuser',
-                    avatar: 'http://89.208.198.186:8080/avatars/default.jpeg',
-                    review: {
-                        author: 'sampleuser',
-                        content: '121',
-                        id: '2',
-                        movie_id: '26',
-                        review_type: 'positive',
-                        title: '213',
-                    },
-                },
-                {
-                    id: 2,
-                    itemType: 'rating',
-                    username: 'sampleuser',
-                    avatar: 'http://89.208.198.186:8080/avatars/default.jpeg',
-                    rating: {
-                        movie_title: 'Матрица',
-                        movie_id: '2',
-                        score: 10,
-                    },
-                },
-            ],
-         */
-        API.getFeed().then((res) => globalEventBus.emit(busEvents.SET_FEED, res.data));
+        API.getFeed().then((res) => {
+            const [reviews, ratings] = res.data;
+            globalEventBus.emit(busEvents.SET_FEED, {
+                'feed': [...reviews, ...ratings],
+            });
+        });
     }
 }
